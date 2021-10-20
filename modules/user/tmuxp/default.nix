c33b233ad833a7d1f1e068c69a55bc02c41ee912
Samuel Ludwig <SLudwig.dev@gmail.com>
2021-10-20 16:04:25 -0400

    Fix improperly-invoked attribute

--- modules/user/tmuxp/default.nix
+++ modules/user/tmuxp/default.nix
@@ -1,6 +1,6 @@
 { pkgs, config, lib, linkConfig, modPath, ... }: {
   home.packages = [ pkgs.tmuxp ];
-  home.programs.tmux.tmuxp.enable = true;
+  programs.tmux.tmuxp.enable = true;
   xdg.configFile."tmuxp".source =
     (linkConfig config).to "${modPath.user}/tmuxp/layouts";
 }
