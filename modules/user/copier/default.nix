b8e1e694bc77a5a601efae40331463062a8bef5c
Samuel Ludwig <SLudwig.dev@gmail.com>
2021-11-03 17:35:27 -0400

    Add copier alias that I hope works and add my templates

--- modules/user/copier/default.nix
+++ modules/user/copier/default.nix
@@ -3,4 +3,11 @@ let
   mkLink = linkConfig config;
   ts = pkgs.tree-sitter.builtGrammars;
   confRoot = "${modPath.user}/copier";
-in { home.packages = [ copier.defaultPackage.${system} ]; }
+in {
+  home.packages = [ copier.defaultPackage.${system} ];
+
+  # Hack referencing pipx-installed version of copier until I can somehow by
+  # some miracle make it an actual working package
+  programs.fish.functions.cop =
+    "~/.local/bin/copier ${confRoot}/templates/$argv[1] $argv[2]";
+}
